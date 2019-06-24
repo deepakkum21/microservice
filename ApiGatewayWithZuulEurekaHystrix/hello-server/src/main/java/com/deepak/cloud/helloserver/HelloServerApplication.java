556bbcfcc8fc4270fb86a3257c8c9663878a95b1
Deepak Kumar <deepak.kumar@sysbiz.org>
2019-06-24 16:00:30 +0530

    hystrix added

---
+++ ApiGatewayWithZuulEurekaHystrix/hello-server/src/main/java/com/deepak/cloud/helloserver/HelloServerApplication.java
@@ -0,0 +1,15 @@
+package com.deepak.cloud.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+
+}
