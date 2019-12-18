f72a7a33f3d3480e01c0e4c63b072fa6f8b09af9
Deepak Kumar <deepak.kumar@sysbiz.com>
2019-12-18 14:44:06 +0530

    started project for

---
+++ MicroserviceWithFault ToleranceAndResilience/ratings-data-service/src/main/java/com/deepak/microservices/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,31 @@
+package com.deepak.microservices.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deepak.microservices.ratingsdataservice.models.Rating;
+import com.deepak.microservices.ratingsdataservice.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+	
+	@GetMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating("1",1);
+	}
+	
+	@GetMapping("/users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		UserRating userRating = new UserRating();
+		List<Rating> ratingList = Arrays.asList(new Rating("1",1));
+		userRating.setUserRating(ratingList);
+		return userRating;
+	}
+
+}
